e62dfca5d63d84d3b45a7aff314f4124c0757df2
ordep <ordep@cachyOS>
2025-08-27 00:35:04 -0300

    Finalizando atividade 11

--- Exercicio_Entrada_e_Saida/src/questoes/exercicio11.java
+++ Exercicio_Entrada_e_Saida/src/questoes/exercicio11.java
@@ -10,15 +10,24 @@ public class exercicio11 {
 	public static void main(String[] args) {
 		Scanner entrada = new Scanner(System.in);
 		
-		int a,b;
+		double x1,x2,y1,y2,ED;
 		
 		System.out.print("Digite um valor para x:");
-		a = entrada.nextInt();
+		x1 = entrada.nextDouble();
 		
 		System.out.println("Digite um valor para y: ");
-		b = entrada.nextInt();
+		x2 = entrada.nextDouble();
 		
-	
+		System.out.println("Digite o valor de y1: ");
+		y1 = entrada.nextDouble();
+		
+		System.out.println("Digite o valor de y2:");
+		y2 = entrada.nextDouble();
+		
+		ED = Math.sqrt(Math.pow(y2 - y1, 2)+ Math.pow(x2 - x1, 2));
+		
+		System.out.print(" A distancia Euclidiana e igual a: "+ED);
+		
 	}
 
 }
